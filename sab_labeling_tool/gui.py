8cb12298d862d49ee954c6361d5813d04dce5526
Héctor Sab <hector.sab@hotmail.com>
2019-02-18 00:47:51 -0600

    Fixed rectangle issues and PopUp added

--- sab_labeling_tool/gui.py
+++ sab_labeling_tool/gui.py
@@ -5,6 +5,8 @@ import tkinter as tk
 from tkinter import ttk
 from PIL import Image, ImageTk
 
+# TODO: Re-write the PopUp method
+
 class Frame:
 	def __init__(self,root=None,main=False,name='Frame',nomain_destroy=False):
 		# Args:
@@ -53,6 +55,116 @@ class Frame:
 		elif self.ismain:
 			# Destroy the window only if its the main window.
 			self.root.destroy()
+
+class PopUp(Frame):
+	# Pop Up object that will be reused instead of being
+	# complitly destroyed each time it's closed.
+	def __init__(self,root=None,main=False,name='Pop Up'):
+		"""
+		Args:
+		  root ():
+		  main ():
+		"""
+		self.root = root; self.main = main; self.name = name
+		self.bbox_label = ''
+		self.tmp_bbox_label = self.bbox_label
+
+		self.edited_object = None
+
+	def create_content(self):
+		"""
+		"""
+		super().__init__(self.root,self.main,self.name)
+		self.entry = tk.Entry(self.frame)
+		self.entry.grid(row=0,column=0,columnspan=2)
+
+		self.btn_ok = tk.Button(self.frame,text='Ok',command=self._ok)
+		self.btn_ok.grid(row=1,column=0)
+		self.btn_cancel = tk.Button(self.frame,text='Cancel',command=self._cancel)
+		self.btn_cancel.grid(row=1,column=1)
+
+		self.entry.insert(0,self.bbox_label)
+
+	def set_initial_position(self):
+		# Locates the PopUp window in the center of the screen
+		#self.root.update() # Forces to generate the shapes
+
+		# Gets frame shape
+		frame_width = self.frame.winfo_width()
+		frame_height = self.frame.winfo_height()
+
+		# Gets screen resolution
+		screen_width = self.root.winfo_screenwidth()
+		screen_height = self.root.winfo_screenheight()
+		x = int(screen_width/2) - int(frame_width/2)
+		y = int(screen_height/2) - int(frame_height/2)
+		self.set_frame_location(x,y)
+
+	def pop(self,obj,text=''):
+		"""
+		"""
+		self.create_content()
+
+		self.frame.bind('<Return>',self._ok_keyboard)
+		self.frame.bind('<Escape>',self._cancel_keyboard)
+		
+		self.edited_object = obj
+		self.change_bbox_label(self.edited_object.name)
+				
+		self.close_frame = False
+		# Pops the frame
+		self.frame.deiconify()
+		# Set focus only on the pop up. Disable all other
+		# windows while the popup is open
+		self.frame.wait_visibility()
+		self.frame.grab_set()
+		self.set_initial_position()
+
+		self.entry.focus()
+
+	def change_bbox_label(self,new_text):
+		self.entry.delete(0, tk.END)
+		self.entry.insert(0,new_text)
+
+		self.tmp_bbox_label = new_text
+
+	def get_bbox_label(self):
+		#
+		return(self.tmp_bbox_label)
+
+	def _ok(self):
+		# Saves the label
+		self.tmp_bbox_label = self.entry.get()
+		self.edited_object.name = self.tmp_bbox_label
+		self._on_closing()
+
+	def _ok_keyboard(self,event):
+		# Ok callback for the keyboard binding
+		self._ok()
+
+	def _cancel_simple(self):
+		# Just cancel the operation,
+		# it does not close the frame
+		self.entry.delete(0, tk.END)
+		self.entry.insert(0,self.tmp_bbox_label)
+
+	def _cancel(self):
+		# Does not save any change, and closes the window
+		self._cancel_simple()
+		self._on_closing()
+
+	def _cancel_keyboard(self,event):
+		# Cancel callback for the keyboard binding
+		self._cancel()
+
+	def _on_closing(self):
+		# Closes the frame and set a flag to close the frame
+		self._cancel_simple()
+		self.close_frame = True
+		#self.frame.withdraw()
+		self.frame.grab_release()
+		
+		self.frame.destroy()
 
 class Point:
 	"""
@@ -195,8 +307,6 @@ class Point:
 		"""
 		self.canvas.delete(self.draw)
 
-
-
 class Rectangle:
 	"""
 	Rectangle tp be drawn in the canvas as bounding box.
@@ -211,6 +321,7 @@ class Rectangle:
 		self.name = name
 		self.canvas = canvas
 		self.selected = False
+		self.exist = True # Indicates if it hasn't been removed
 
 		self.previous_mouse_possition = None #[x,y]
 		
@@ -280,11 +391,13 @@ class Rectangle:
 	def set_selection_on(self):
 		"""
 		"""
+		self.canvas.itemconfig(self.draw, outline='blue')
 		self.selected = True
 
 	def set_selection_off(self):
 		"""
 		"""
+		self.canvas.itemconfig(self.draw, outline='red')
 		self.selected = False
 
 	def get_coord(self):
@@ -462,24 +575,71 @@ class Rectangle:
 				self.change_coord(rect_coord)
 				self.adjust_control_points()
 
+	def hide(self):
+		"""
+		Hides the object
+		"""
+		self.hide_control_points()
+		self.canvas.itemconfigure(self.draw, state='hidden')
+
+	def expose(self):
+		"""
+		Shows the object if previously hidden
+		"""
+		self.canvas.itemconfigure(self.draw, state='normal')
+		self.expose_control_points()
+
+	def set_removed_on(self):
+		"""
+		It indicates that it has been removed from the existing objects
+		and should not be considered in the following tasks.
+		"""
+		self.exist = False
+	
 	def destroy(self):
 		"""
 		Destroy the object
 		"""
+		for point in self.points:
+			point.destroy()
+
 		self.canvas.delete(self.draw)
 
 
 class ImageFrame(Frame):
 	"""
 	"""
-	def __init__(self,root=None,main=False,name='Image Frame'):
+	def __init__(self,root=None,main=False,name='Image Frame',
+		def_rect_name='New_Object',popup=None):
+		"""
+		Args:
+		  root ():
+		  main (bool): Indicates if this is the main Frame.
+		  name (str): Name of the frame.
+		  def_rect_name (str): Default rectangles name.
+		  popup (PopUp): Pop Up window
+		"""
 		super().__init__(root,main,name)
 		self.objects = [] # Contains all the rectangles
+		self.previous_mouse_possition = None #[x,y]
+		self.tmp_rectangle = None
+		self.popup = popup
+
+		# Original coord of the rectangles when first loaded
+		self.original_objs = None 
+
+		# Functionality for the main GUI
+		self.FLAG_NEXT = False 
+		self.FLAG_PREV = False
+
+		self.def_rect_name = def_rect_name
 
 		self.create_content()
 
 	def create_content(self):
-		# Creates all the content of the image frame
+		"""
+		Creates all the content of the image frame
+		"""
 		shape = (480,640) # Default shape
 		# Creates the canvas where everything will be displayed
 		self.canvas = tk.Canvas(self.frame,width=shape[1],height=shape[0],
@@ -495,23 +655,194 @@ class ImageFrame(Frame):
 		# Initialize the image in the canvas
 		self.image = self.canvas.create_image(0,0,anchor=tk.NW,image=self.im_data)
 
-		# Fix frame size
+		# Fixes frame size
 		self.frame.resizable(width=False, height=False)
 
-		self.canvas.tag_bind(self.image,'<Button-1>',self.__image_pressed)
+		#self.canvas.tag_bind(self.image,'<Button-1>',self.__image_pressed)
 
-	def __image_pressed(self,event):
+		self.canvas.tag_bind(self.image,'<Button-1>',self.__object_clicked)
+		self.canvas.tag_bind(self.image,'<ButtonRelease-1>',self.__object_unclicked)
+		self.canvas.tag_bind(self.image,'<B1-Motion>',self.__object_moving)
+
+		# Standard binds across Frames
+		self.frame.bind('a',self.__add)
+		self.frame.bind('m',self.__modify)
+		self.frame.bind('x',self.__remove)
+		self.frame.bind('r',self.__reset)
+		self.frame.bind('n',self.__next)
+		self.frame.bind('p',self.__previous)
+
+	def __object_clicked(self,event):
+		"""
+		"""
 		print('Canvas Pressed')
-		#Point(self.canvas,[200,200],mode='v-edge')
-		Rectangle(self.canvas,[200,200,400,400])
+		self.previous_mouse_possition = np.array([event.x,event.y])
 
-	def load_new_data(self,im,objs):
-		# 
+	def __object_unclicked(self,event):
+		"""
+		"""
+		# Destroy the tmp_rectangle if the canvas was clicked and
+		# unclicked at the same position
+		if (self.tmp_rectangle is not None and 
+			self.previous_mouse_possition[0]-event.x==0 and
+			self.previous_mouse_possition[1]-event.y==0):
+			
+			self.tmp_rectangle.destroy()
+			self.tmp_rectangle = None
+		
+		if (self.previous_mouse_possition[0]-event.x==0 and
+			self.previous_mouse_possition[1]-event.y==0):
+
+			self.deselect_all_objects()
+
+		# Erase the previous mouse position
+		self.previous_mouse_possition = None
+
+	def __object_moving(self,event):
+		"""
+		"""
+		# In order to have a smooth animation, destroy 
+		# the previous tmp_rectangle
+		if self.tmp_rectangle is not None:
+			self.tmp_rectangle.destroy()
+
+		# Order the parameters so they are in the following
+		# format: [x_min,y_min,x_max,y_max]
+		coord = [0,0,0,0]
+		if self.previous_mouse_possition[0]>event.x:
+			coord[0] = event.x
+			coord[2] = self.previous_mouse_possition[0]
+		else:
+			coord[2] = event.x
+			coord[0] = self.previous_mouse_possition[0]
+
+		if self.previous_mouse_possition[1]>event.y:
+			coord[3] = event.y
+			coord[1] = self.previous_mouse_possition[1]
+		else:
+			coord[1] = event.y
+			coord[3] = self.previous_mouse_possition[1]
+
+		# Ensure that the rectangle is inside the canvas
+		canvas_shape = [self.canvas.winfo_width(),self.canvas.winfo_height()]
+		canvas_shape = np.array(canvas_shape)
+
+		if coord[2]>(canvas_shape[0]-1):
+			# If we are trying to move ouside the right limit
+			coord[2] = canvas_shape[0]-1
+
+		if coord[3]>(canvas_shape[1]-1):
+			# If we are trying to move ouside the bottom limit
+			coord[3] = canvas_shape[1]-1
+
+		if coord[0]<0:
+			# If we are trying to move ouside the left limit
+			coord[0] = 0
+
+		if coord[1]<0:
+			# If we are trying to move ouside the top limit
+			coord[1] = 0
+
+		# Create a new tmp_rectangle
+		self.tmp_rectangle = Rectangle(self.canvas,coord,name=self.def_rect_name)
+
+	def __add(self,event):
+		"""
+		Adds the tmp_rectangle to the list of objects
+		"""
+		self.objects.append(self.tmp_rectangle)
+		self.tmp_rectangle = None
+		print('ADD')
+
+	def __modify(self,event):
+		"""
+		Modify the name of the selected object
+		"""
+		if self.popup is not None:
+			for obj in self.objects:
+				if obj.selected:
+					self.popup.pop(obj)
+
+		print('MODIFY')
+
+	def __remove(self,event):
+		"""
+		Removes and objects from the canvas
+		"""
+		for obj in self.objects:
+			if obj.selected:
+				obj.destroy()
+				obj.set_selection_off()
+				obj.set_removed_on()
+
+		print('REMOVE')
+
+	def __reset(self,event):
+		"""
+		Resets the objects in the canvas to the original ones that where loaded
+		at first with the image.
+		"""
+		self.destroy_all_objects()
+		if self.original_objs is not None:
+			self.draw_new_objects(self.original_objs)
+		print('RESET')
+	
+	def __next(self,event):
+		"""
+		"""
+		self.set_next_image_on()
+		print('NEXT')
+
+	def __previous(self,event):
+		"""
+		"""
+		self.set_prev_image_on()
+		print('PREVIOUS')
+
+	def set_next_image_on(self):
+		"""
+		"""
+		self.FLAG_NEXT = True
+
+	def set_next_image_off(self):
+		"""
+		"""
+		self.FLAG_NEXT = False
+
+	def set_prev_image_on(self):
+		"""
+		"""
+		self.FLAG_PREV = True
+
+	def set_prev_image_off(self):
+		"""
+		"""
+		self.FLAG_PREV = False
+
+	def deselect_all_objects(self):
+		"""
+		"""
+		for obj in self.objects:
+			obj.set_selection_off()
+
+	def load_new_data(self,im,objs=None):
+		"""
+		Displays new data into the canvas
+		Args:
+		  im (np.ndarray): Canvas Image
+		  objs (None|list): List of coordenates of rectangles with
+		    format [x_min,y_min,x_max,y_max]. If None, it will ignore
+		    the drawing of objects method.
+		"""
 		self.set_new_image_on_canvas(im)
-		self.draw_new_objects(objs)
+		self.original_objs = objs # 
+		if objs is not None:
+			self.draw_new_objects(objs)
 
 	def set_new_image_on_canvas(self,im):
-		# Converst the image to a format that tkinter can handle
+		"""
+		Converst the image to a format that tkinter can handle
+		"""
 		self.shape = (im.shape[0],im.shape[1])
 		self.canvas.config(height=im.shape[0],width=im.shape[1])
 		self.im_data = ImageTk.PhotoImage(image=Image.fromarray(im))
@@ -519,17 +850,35 @@ class ImageFrame(Frame):
 		self.canvas.itemconfig(self.image,image=self.im_data)
 
 	def draw_new_objects(self,objs):
+		"""
+		Adds new objects to the canvas.
+		Args:
+		  objs (list): Lost of coordenates of the rectangles with
+		    format [x_min,y_min,x_max,y_max]
+		"""
+		self.destroy_all_objects()
 		for obj in objs:
-			self.objects.append(Rectangle(self.canvas,obj))
+			self.objects.append(Rectangle(self.canvas,obj,self.def_rect_name))
 
 		#print(self.objects[0].object.cget("text"))
+
+	def destroy_all_objects(self):
+		"""
+		Destroy all objects in the canvas.
+		"""
+		for obj in self.objects:
+			obj.destroy()
+
+		self.objects = []
 
 if __name__=='__main__':
 	im_path = '../dog.jpg'
 	im = cv2.imread(im_path)[...,::-1]
 
-	imFrame = ImageFrame(main=True)
-	imFrame.set_new_image_on_canvas(im)
+	root = tk.Tk()
+	popup = PopUp(root=root)
+	imFrame = ImageFrame(root=root,main=True,popup=popup)
+	imFrame.load_new_data(im)
 
 	while True:
 		# Update GUI
